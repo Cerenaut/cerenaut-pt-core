186114fd23d4e40a0e40f1bed8ca684872593897
Abdelrahman Ahmed <181392+abdel@users.noreply.github.com>
2021-10-01 10:36:07 +1000

    add sync.sh

---
+++ sync.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+################################################################################
+# sync code
+################################################################################
+
+export MACHINE=${1:-incbox-tb}
+DEST_DIR='~/agief-remote-run/cerenaut-pt-core'
+
+# sync this folder
+cmd="rsync --chmod=ug=rwX,o=rX --perms -av ./ $MACHINE:$DEST_DIR --exclude='.git/' --filter=':- .gitignore'"
+echo $cmd
+eval $cmd
+status=$?
+
+if [ $status -ne 0 ]
+then
+  echo "ERROR:  Could not complete rsync operation - failed at 'sync this folder' stage." >&2
+  echo "	Error status = $status" >&2
+  echo "	Exiting now." >&2
+  exit $status
+fi
